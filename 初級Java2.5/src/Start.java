45cb70c2b9930753a56ab97faade8b207420f177
asahi <asahi5286@gmail.com>
2024-05-14 10:46:33 +0900

    初級Java3.1

--- 初級Java2.5/src/Start.java
+++ 初級Java2.5/src/Start.java
@@ -3,14 +3,8 @@ public class Start {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		int x = 2;
-		int y = 3;
-		System.out.println(x*y);
-		
-		MyFrame frame1 = new RectFrame();
-		frame1.setLocation(300,300);
-		MyFrame frame2 = new RectFrame();
-		frame2.setLocation(600,300);
+		System.out.println("プログラムを実行します");
+		MyFrame frame1 =new RectTest();
 		
 	}
 
