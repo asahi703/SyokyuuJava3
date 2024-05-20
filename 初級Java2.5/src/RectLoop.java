b96731e8fe9bcc627bc67a0ffc99e16b24e12f37
asahi <asahi5286@gmail.com>
2024-05-20 11:44:33 +0900

    Revert "初級Java4.3"
    This reverts commit 72909c63ac6ec4e4b3d7e00e943aa877e43f3a6d.

--- 初級Java2.5/src/RectLoop.java
+++ 初級Java2.5/src/RectLoop.java
@@ -2,8 +2,8 @@
 public class RectLoop extends MyFrame{
 	public void run() {
 		setColor(0,128,0);
-		int x =30;
-		for(int i = 0; i < 10; i++) {
+		int x=30;
+		while(x<=210) {
 			System.out.println(x);
 			fillRect(x,80,10,100);
 			x+=20;
