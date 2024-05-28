1b394ca7e3ed1e19d46ab98801561f83842c2a2c
asahi <asahi5286@gmail.com>
2024-05-28 10:08:48 +0900

    初級Java6.2

--- 初級Java2.5/src/DisplayFaceObject.java
+++ 初級Java2.5/src/DisplayFaceObject.java
@@ -1,4 +1,12 @@
 
-public class DisplayFaceObject {
+public class DisplayFaceObject extends MyFrame{
+	public void run() {
+		Face face1=new Face(50,50);
+		face1.draw(this);
+		
+		Face face2=new Face(200,100);
+		face2.draw(this);
+	}
 
 }
+
