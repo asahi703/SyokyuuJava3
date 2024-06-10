62f68584a9df518b16ea4ac47777db035fbd5b4f
asahi <asahi5286@gmail.com>
2024-06-10 11:35:06 +0900

    練習問題8-1 上げなおし

--- 初級Java2.5/src/DisplayTruck.java
+++ 初級Java2.5/src/DisplayTruck.java
@@ -1,7 +1,21 @@
+import java.util.Vector;
 
 public class DisplayTruck extends MyFrame{
 	public void run() {
-		Truck truck1=new Truck(30,150,3,0);
-		truck1.draw(this);
+		Vector<Vehicle> vehicles=new Vector<Vehicle>();
+		vehicles.add(new Car(10,30,3,0));
+		vehicles.add(new Car(300,100,-5,0));
+		vehicles.add(new Train(10,170,7,0));
+		vehicles.add(new Train(300,230,-7,0));
+		vehicles.add(new Truck(10,280,3,0));
+		vehicles.add(new Truck(10,330,7,0));
+		for(int i=0; i<30; i++) {
+			clear();
+			for(int j=0; j<vehicles.size(); j++) {
+				vehicles.get(j).draw(this);
+				vehicles.get(j).move();
+			}
+			sleep(0.1);
+		}
 	}
 }
